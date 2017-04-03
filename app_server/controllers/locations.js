bbaf23c6735054d02d22d80b79cb4529d6fb7e58
Kadin <kadinvanvalin@yahoo.com>
2017-04-03 19:17:04 -0400

    second commit

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Kadin on 4/1/2017.
+ */
+/** GET 'home' page **/
+
+module.exports.homelist = function(req, res) {
+    res.render('locations-list', {title : 'Home'});
+};
+
+/** GET 'location info' page **/
+module.exports.locationInfo = function(req, res) {
+  res.render('index', {title : 'Location info'});
+};
+
+/** GET 'add review' page **/
+module.exports.addReview = function(req, res){
+    res.render('index', { title: 'Add review'});
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,17 @@
+/**
+ * Created by Kadin on 4/1/2017.
+ */
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require("../controllers/locations");
+var ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
